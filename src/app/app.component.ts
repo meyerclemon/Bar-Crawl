1a745d84f8345f09f6379706694d007b324458f7
Zema <zebede15@gmail.com>
2019-06-24 10:24:30 -0700

    search function works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-
-declare var H: any;
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-root',
@@ -9,30 +7,12 @@ declare var H: any;
 })
 export class AppComponent implements OnInit {
 
-    private platform: any;
-
-    @ViewChild("map", {static: false})
-    public mapElement: ElementRef;
+    public query: string;
 
     public constructor() {
-        this.platform = new H.service.Platform({
-            "app_id": "DEd7oRQY19pWOmIZlVd3",
-            "app_code": "fUHqreQHv4u572-OKbhcHg"
-        });
+        this.query = "starbucks";
     }
 
     public ngOnInit() { }
-
-    public ngAfterViewInit() {
-        let defaultLayers = this.platform.createDefaultLayers();
-        let map = new H.Map(
-            this.mapElement.nativeElement,
-            defaultLayers.normal.map,
-            {
-                zoom: 15,
-                center: { lat: 47.60188, lng: -122.33402 }
-            }
-        );
-    }
 
 }
