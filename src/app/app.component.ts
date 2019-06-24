5215c848f6c89e85a4025d0a28bfa05dd084794a
Zema <zebede15@gmail.com>
2019-06-24 09:41:14 -0700

    Follow tutorial, make center coordinates located at Epicodus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var H: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'here-project';
+export class AppComponent implements OnInit {
+
+    private platform: any;
+
+    @ViewChild("map", {static: false})
+    public mapElement: ElementRef;
+
+    public constructor() {
+        this.platform = new H.service.Platform({
+            "app_id": "DEd7oRQY19pWOmIZlVd3",
+            "app_code": "fUHqreQHv4u572-OKbhcHg"
+        });
+    }
+
+    public ngOnInit() { }
+
+    public ngAfterViewInit() {
+        let defaultLayers = this.platform.createDefaultLayers();
+        let map = new H.Map(
+            this.mapElement.nativeElement,
+            defaultLayers.normal.map,
+            {
+                zoom: 15,
+                center: { lat: 47.60188, lng: -122.33402 }
+            }
+        );
+    }
+
 }
